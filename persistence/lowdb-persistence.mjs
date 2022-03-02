2abecece031914ceaef78be8883422c18288050b
John Paxton <johnpaxton@gmail.com>
2022-03-02 08:24:04 -0500

    Rename persistence to use ESM

--- persistence/lowdb-persistence.mjs
+++ persistence/lowdb-persistence.mjs
@@ -1,14 +1,19 @@
 import { join, dirname } from 'path';
 import { Low, JSONFile } from 'lowdb';
 import { fileURLToPath } from 'url';
+import * as lodash from 'lodash-es';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const filename = 'books.json';
 
+class LowWithLodash extends Low {
+  chain = lodash.chain(this).get('data');
+}
+
 // Use JSON file for storage
 const file = join(__dirname, filename);
 const adapter = new JSONFile(file);
-const db = new Low(adapter);
+const db = new LowWithLodash(adapter);
 
 await db.read();
 
