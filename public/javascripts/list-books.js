47cf46e416db551a3c78b37669dfab914347bb91
Mary <55006288+mwishart@users.noreply.github.com>
2022-03-02 11:02:47 -0500

    Update list-books.js

--- public/javascripts/list-books.js
+++ public/javascripts/list-books.js
@@ -1,3 +1,24 @@
 console.log('Success!');
 
+
+let restServer = 'http://localhost:3000/books';
+
+async function fetchData(url){
+    let results = [];
+    try{
+        let response = await fetch(restServer);
+        if(response.ok){
+        let results = await fetch(restServer).json();
+        console.log('Got results:', results);
+        }
+    } catch (error) {
+        console.error("Couldn't fetch data because ${error}");
+    }
+    
+}
+function renderTable(books){
+let tableBody = document.querySelector('#books-container tbody');
+}
+
+fetchData(restServer);
 export {};
