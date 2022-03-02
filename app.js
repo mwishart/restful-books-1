bba3023a4d6a43a3c97c1f8a808f48d3df8d9ccc
John Paxton <johnpaxton@gmail.com>
2022-03-02 08:24:19 -0500

    Remove unused routes

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var indexRouter = require('./routes/index');
+const booksRouter = require('./routes/books');
 
 var app = express();
 
 app.use(function (request, response, next) {
-  console.log("Custom middleware");
+  console.log('Custom middleware');
   next();
 });
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use('/', indexRouter);
+app.use('/books', booksRouter);
 
 module.exports = app;
