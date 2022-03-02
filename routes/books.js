69c6df53a1a16d0a19d820ad45dcf2a74eef2c40
John Paxton <johnpaxton@gmail.com>
2022-03-02 10:10:05 -0500

    Return books as string

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+const books = [
+  {
+    id: '1',
+    title: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald',
+    authorId: '1',
+    year: 1925,
+  },
+  {
+    id: '2',
+    title: 'Clifford, the Big Red Dog',
+    author: 'Norman Bridwell',
+    authorId: '2',
+    year: 1963,
+  },
+];
+
+// GET /books/ -> [ array of books ]
+router.get('/', (req, res) => {
+  let bookList = '';
+  for (let book of books) {
+    // bookList += `${book.title} by ${book.author}<br>`;
+    bookList = bookList + `${book.title} by ${book.author}<br>`;
+  }
+  res.send(`Here's an array of books: ${bookList}`);
+});
+
 router.get('/gatsby', (req, res) => {
   res.send('The Great Gatsby');
 });
