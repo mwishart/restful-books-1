2764bf5bb589e773fe3dae7866525609ff7e7b49
John Paxton <johnpaxton@gmail.com>
2022-03-07 09:25:23 -0500

    Modify data structure

--- sql/demos-schema.sql
+++ sql/demos-schema.sql
@@ -4,12 +4,14 @@ create schema demos;
 
 use demos;
 
-create table demos.students (
+create table students (
   first_name varchar(100) not null,
   last_name varchar(100) not null,
+  cohort int,
+  start_date date,
   email varchar(200),
   github_id varchar(100),
-  location varchar(10) not null,
-  id int not null auto_increment,
-  primary key(id)
+  country varchar(10) not null,
+  student_id int not null auto_increment,
+  primary key(student_id)
 );
