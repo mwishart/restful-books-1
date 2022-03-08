cb7bc8df9783ffe8b90769e392e6d0c31c49675c
John Paxton <johnpaxton@gmail.com>
2022-03-08 08:07:08 -0500

    Add teams-students table

--- sql/demos-schema.sql
+++ sql/demos-schema.sql
@@ -53,3 +53,89 @@ INSERT INTO teams (team_id, team_name) VALUES (2, 'Bloons');
 INSERT INTO teams (team_id, team_name) VALUES (3, 'Civilization');
 INSERT INTO teams (team_id, team_name) VALUES (4, 'Halo');
 INSERT INTO teams (team_id, team_name) VALUES (5, 'Kingdom Hearts');
+
+create table teams_students (
+  team_id int not null,
+  student_id int not null,
+  week int not null,
+  teams_students_id int not null,
+  constraint PK_teams_students_id primary key (teams_students_id),
+  constraint FK_team_id foreign key(team_id) references teams(team_id),
+  constraint FK_student_id foreign key(student_id) references students(student_id);
+);
+
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 1, 1, 19 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 1, 1, 12 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 1, 1, 14 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 1, 1, 23 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 1, 1, 20 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 1, 2, 21 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 1, 2, 5 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 1, 2, 7 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 1, 2, 25 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 1, 2, 18 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 1, 3, 16 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 1, 3, 9 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 1, 3, 13 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 1, 3, 2 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 1, 3, 14 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 2, 1, 9 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 2, 1, 5 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 2, 1, 22 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 2, 1, 13 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 2, 1, 1 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 2, 2, 11 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 2, 2, 10 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 2, 2, 19 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 2, 2, 4 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 2, 2, 24 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 2, 3, 23 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 2, 3, 4 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 2, 3, 1 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 2, 3, 7 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 2, 3, 24 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 3, 1, 21 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 3, 1, 10 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 3, 1, 6 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 3, 1, 7 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 3, 1, 24 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 3, 2, 16 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 3, 2, 23 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 3, 2, 9 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 3, 2, 13 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 3, 2, 17 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 3, 3, 3 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 3, 3, 8 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 3, 3, 19 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 3, 3, 20 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 3, 3, 18 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 4, 1, 16 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 4, 1, 2 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 4, 1, 25 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 4, 1, 18 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 4, 1, 17 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 4, 2, 8 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 4, 2, 22 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 4, 2, 12 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 4, 2, 15 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 4, 2, 3 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 4, 3, 11 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 4, 3, 21 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 4, 3, 6 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 4, 3, 12 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 4, 3, 17 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 5, 1, 11 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 5, 1, 8 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 5, 1, 4 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 5, 1, 15 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 5, 1, 3 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 5, 2, 6 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 5, 2, 1 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 5, 2, 20 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 5, 2, 2 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 5, 2, 14 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 5, 3, 22 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 5, 3, 5 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 5, 3, 15 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 5, 3, 25 );
+INSERT INTO teams_students (team_id, week, student_id) VALUES ( 5, 3, 10 );
