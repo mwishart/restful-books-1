19106b5c5e94c1692a81c1c15a314198528adf0f
John Paxton <johnpaxton@gmail.com>
2022-03-17 09:35:46 -0400

    Add comments and more expectations

--- test/book-model-test.js
+++ test/book-model-test.js
@@ -3,6 +3,7 @@ const { Author, Book, Library } = require('../orm/books/books-models');
 const expect = require('chai').expect;
 
 describe('Book Model', () => {
+  // it(description, functionWithTestCode)
   it('Find at least one book', async () => {
     let books = await Book.findAll({ logging: false });
     expect(books).to.have.lengthOf.at.least(5);
@@ -11,13 +12,17 @@ describe('Book Model', () => {
   it('Find some Books', async () => {
     let testTitle = 'Harry Potter';
     let books = await Book.findAll({
-      logging: false,
+      // logging: false,
+      // Generates: SELECT * FROM books WHERE title like '%testTitle%'
       where: {
-        title: { [Op.substring]: testTitle },
+        title: { [Op.regexp]: testTitle },
       },
     });
     expect(books).to.exist;
     expect(books).to.have.lengthOf.at.least(2);
+    for (let book of books) {
+      expect(book.title).to.contain(testTitle);
+    }
   });
 
   it('Check Author associations lazily', async () => {
